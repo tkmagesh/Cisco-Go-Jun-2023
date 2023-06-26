1d59ac52d470be397e2904ed26f440d9a9f4f898
Magesh Kuppan <tkmagesh77@gmail.com>
2023-06-26 14:32:13 +0530

    assignment-01 solution

--- 02-programming-construct/01-assignment.go
+++ 02-programming-construct/01-assignment.go
@@ -1,1 +1,16 @@
 /* Print all the prime numbers between 3 to 100 */
+package main
+
+import "fmt"
+
+func main() {
+OUTER_LOOP:
+	for no := 3; no <= 100; no++ {
+		for i := 2; i <= (no / 2); i++ {
+			if no%i == 0 {
+				continue OUTER_LOOP
+			}
+		}
+		fmt.Printf("%d is a prime no\n", no)
+	}
+}
