801ee9b587ffbe5c358fd234a7e064b4449adec6
Magesh Kuppan <tkmagesh77@gmail.com>
2023-06-26 12:20:01 +0530

    variable declarations

---
+++ 01-intro/02-variables.go
@@ -0,0 +1,118 @@
+package main
+
+import "fmt"
+
+// Cannot use := at package level
+// age := 30
+
+// Can have unused variables at package level
+var age = 30
+
+func main() {
+
+	/*
+		var name string
+		name = "Magesh"
+		// print(name + "\n")
+		// fmt.Println(name)
+		fmt.Printf("Hi %s, Have a nice day!\n", name)
+	*/
+
+	/*
+		var name string = "Magesh"
+		fmt.Printf("Hi %s, Have a nice day!\n", name)
+	*/
+
+	//type inference
+	/*
+		var name = "Magesh"
+		fmt.Printf("Hi %s, Have a nice day!\n", name)
+	*/
+
+	//using :=
+	name := "Magesh"
+	fmt.Printf("Hi %s, Have a nice day!\n", name)
+
+	//cannot have unused variables
+	// age := 30
+
+	// Using multiple variables
+	/*
+		var x int
+		var y int
+		var result int
+		var str string
+		x = 100
+		y = 200
+		result = x + y
+		str = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var x, y, result int
+		var str string
+		x = 100
+		y = 200
+		result = x + y
+		str = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var (
+			x, y, result int
+			str          string
+		)
+		x = 100
+		y = 200
+		result = x + y
+		str = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	// Declaration & initialization
+	/*
+		var x int = 100
+		var y int = 200
+		var result int = x + y
+		var str string = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var (
+			x      int    = 100
+			y      int    = 200
+			result int    = x + y
+			str    string = "Sum of %d and %d is %d\n"
+		)
+	*/
+	/*
+		var (
+			x, y   int    = 100, 200
+			result int    = x + y
+			str    string = "Sum of %d and %d is %d\n"
+		)
+	*/
+
+	/*
+		var (
+			x, y   = 100, 200
+			result = x + y
+			str    = "Sum of %d and %d is %d\n"
+		)
+	*/
+
+	/*
+		var (
+			x, y, str = 100, 200, "Sum of %d and %d is %d\n"
+			result    = x + y
+		)
+	*/
+
+	x, y, str := 100, 200, "Sum of %d and %d is %d\n"
+	result := x + y
+
+	fmt.Printf(str, x, y, result)
+}
--- 01-intro/notes.md
+++ 01-intro/notes.md
@@ -45,3 +45,37 @@ func main() {
 
 ## To create a build for different platform ##
 -  GOARCH=386 GOOS=windows go build 01-hello-world.go 
+
+# Data Types #
+- string
+- bool
+- Integer family
+	- int
+	- int8
+	- int16
+	- int32
+	- int64
+- Unsigned Integer family
+	- uint
+	- uint8
+	- uint16
+	- uint32
+	- uint64
+- Float family
+	- float32
+	- float64
+- Complex number family
+	- complex64 ( real [float32], imaginary [float32])
+	- complex128 ( real [float64], imaginary [float64])
+- Type aliases
+	- byte (alias for uint8)
+	- rune (alias for int32) (unicode code point)
+
+## Variable Declarations ##
+### function scope ###
+- Can use ":=" in a function
+- Cannot have unused variables in a function
+
+### package scope ###
+- Cannot use ":=" at package level
+- Can have unused variables at package level
