2f3d33393940fc5f3cadd9989607eb66c617b053
Magesh Kuppan <tkmagesh77@gmail.com>
2023-06-26 14:42:41 +0530

    using fmt.scan & assignment-02

---
+++ 02-programming-construct/fmt-scan.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		var name string
+		fmt.Println("Enter your name :")
+		fmt.Scanln(&name)
+		fmt.Println(name)
+	*/
+
+	var fname, lname string
+	fmt.Println("Enter your firstname & lastname")
+	fmt.Scanln(&fname, &lname)
+	fmt.Scanf("%s %s\n", &fname, &lname)
+
+	/*
+		fmt.Println("Enter your lastname, firstname")
+		fmt.Scanf("%s, %s\n", &lname, &fname)
+		fmt.Println(fname, lname)
+	*/
+}
