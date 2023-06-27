9e2e8535c7427e0e05b30c5c8aba1a410e47ff4b
Magesh Kuppan <tkmagesh77@gmail.com>
2023-06-27 11:43:44 +0530

    pointers

---
+++ 07-pointers/program.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	var no int
+	no = 100
+
+	var noPtr *int
+	noPtr = &no // get the address of "no" variable
+	fmt.Println(noPtr, no)
+
+	//dereferencing - accessing the value at an address(pointer)
+	valAtNoPtr := *noPtr
+	fmt.Println(valAtNoPtr)
+
+	fmt.Println(no == *(&no))
+	fmt.Println("Before incrementing : no =", no)
+	increment(&no)
+	fmt.Println("After incrementing : no =", no)
+
+	n1, n2 := 100, 200
+	fmt.Printf("Before swapping, n1 = %d and n2 = %d\n", n1, n2)
+	swap( /*  */ )
+	fmt.Printf("After swapping, n1 = %d and n2 = %d\n", n1, n2)
+
+}
+
+func increment(x *int) {
+	fmt.Println(x)
+	*x++
+}
+
+func swap() /* do not return anything */ {
+
+}
