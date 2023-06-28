ca48cb1b27143b96bc87e3347679f144d161aa60
Magesh Kuppan <tkmagesh77@gmail.com>
2023-06-28 15:12:09 +0530

    handling multiple channels

---
+++ 14-concurrency/15-demo.go
@@ -0,0 +1,41 @@
+/* handling multiple channels (using select-case) */
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	ch3 := make(chan int)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch1 <- 100
+	}()
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		d3 := <-ch3
+		fmt.Println("Data from ch3 : ", d3)
+	}()
+
+	go func() {
+		time.Sleep(4 * time.Second)
+		ch2 <- 200
+	}()
+
+	for i := 0; i < 3; i++ {
+		select {
+		case d1 := <-ch1:
+			fmt.Println(d1)
+		case ch3 <- 300:
+			fmt.Println("data sent to ch3")
+		case d2 := <-ch2:
+			fmt.Println(d2)
+		}
+	}
+}
